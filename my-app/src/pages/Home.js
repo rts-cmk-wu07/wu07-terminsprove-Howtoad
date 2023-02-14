ee0f842edc1e35b6554dd1339e92cccbdd0599e8
Kim <howtoad123@gmail.com>
2023-02-14 15:49:21 +0100

    printing a random class with the background picutre as the image for the class and the name of the class on top of it

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,10 +1,14 @@
+import BigClass from "../components/BigClass";
 import MobileNav from "../components/MobileNav";
 
 const Home = () => {
   return (
-    <MobileNav>
-      <h1>Popular classes</h1>
-    </MobileNav>
+    <>
+      <MobileNav>
+        <h1>Popular classes</h1>
+      </MobileNav>
+      <BigClass></BigClass>
+    </>
   );
 };
 
